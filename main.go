7a262c31204efe03637ad486cf9afc95927f9d4b
Mahendra Dwi Purwanto <mahendradwipurwanto@gmail.com>
2023-07-24 17:18:22 +0700

    Add inlince comment

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ import (
 	"github.com/joho/godotenv"
 )
 
+/**
+ * Func: InsertDataFromFile is for read the json file containing json array data
+ *
+ */
 func InsertDataFromFile(db *sql.DB, filePath string, columnMapping map[string]string, table string) error {
 	jsonData, err := ioutil.ReadFile(filePath)
 	if err != nil {
@@ -22,16 +26,42 @@ func InsertDataFromFile(db *sql.DB, filePath string, columnMapping map[string]st
 	return InsertData(db, jsonData, columnMapping, table)
 }
 
+/**
+ * Func: joinStrings is for joining the string for raw query
+ *
+ * @author Mahendra Dwi Purwanto
+ *
+ */
+
+func joinStrings(slice []string, sep string) string {
+	if len(slice) == 0 {
+		return ""
+	}
+	if len(slice) == 1 {
+		return slice[0]
+	}
+	return slice[0] + sep + joinStrings(slice[1:], sep)
+}
+
+/**
+ * Func: InsertData is for inserting the data into the database
+ *
+ * @author Mahendra Dwi Purwanto
+ *
+ */
 func InsertData(db *sql.DB, jsonData []byte, columnMapping map[string]string, table string) error {
+	// Init variable
 	var dataSlice []map[string]interface{}
+	var placeholders, columns []string
+	var values []interface{}
+
+	// Decode Json and set it to dataSlice
 	err := json.Unmarshal(jsonData, &dataSlice)
 	if err != nil {
 		return err
 	}
 
-	var placeholders, columns []string
-	var values []interface{}
-
+	// Build column and values for raw query
 	for _, data := range dataSlice {
 		var valueStrings []string
 		for jsonKey, columnName := range columnMapping {
@@ -46,8 +76,10 @@ func InsertData(db *sql.DB, jsonData []byte, columnMapping map[string]string, ta
 		placeholders = append(placeholders, "("+joinStrings(valueStrings, ", ")+")")
 	}
 
+	// Set query
 	query := fmt.Sprintf("INSERT INTO %s (%s) VALUES %s", table, strings.Join(columns, ", "), joinStrings(placeholders, ", "))
 
+	// Exec Query
 	_, err = db.Exec(query, values...)
 	if err != nil {
 		return err
@@ -56,37 +88,32 @@ func InsertData(db *sql.DB, jsonData []byte, columnMapping map[string]string, ta
 	return nil
 }
 
-func joinStrings(slice []string, sep string) string {
-	if len(slice) == 0 {
-		return ""
-	}
-	if len(slice) == 1 {
-		return slice[0]
-	}
-	return slice[0] + sep + joinStrings(slice[1:], sep)
-}
-
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Setting up connection
 	db, err := sql.Open("mysql", os.Getenv("DB_USERNAME")+":"+os.Getenv("DB_PASSWORD")+"@tcp("+os.Getenv("DB_HOST")+":"+os.Getenv("DB_PORT")+")/"+os.Getenv("DB_NAME"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
+	// Set the table name
+	table := "tb_agama"
+
+	// Get the file path
 	filePath := "assets/example.json"
 
+	// Mapping connection between key json to column name in table
 	columnMapping := map[string]string{
 		"id":         "id",
 		"nama_agama": "name",
 	}
 
-	table := "tb_agama"
-
+	// Insert data handler
 	err = InsertDataFromFile(db, filePath, columnMapping, table)
 	if err != nil {
 		log.Fatal(err)
